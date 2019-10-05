7cf52525e357bffa1e4db78b07214a20674f6920
VyacheslavGL <48326907+VyacheslavGL@users.noreply.github.com>
2019-10-05 13:10:22 +0300

    Add files via upload

---
+++ Lesson-03/README.md
@@ -0,0 +1,11 @@
+# Компоненты, переиспользование и декомпозиция
+Документация:
+* https://ru.vuejs.org/v2/guide/components.html
+
+Компоненты для примеров:
+* https://www.npmjs.com/package/vuejs-datepicker
+* https://github.com/tinymce/tinymce-vue
+
+P.S. Ссылка на unpkg для TinyMCE
+https://unpkg.com/@tinymce/tinymce-vue@2.1.0/lib/browser/tinymce-vue.min.js
+
---
+++ Lesson-03/posts/app.js
@@ -0,0 +1,66 @@
+Vue.component('v-alert', {
+    template: "#alert-template",
+    //здесь type - название атрибута (пропса)
+    props: {
+        type: {
+            default: "info",
+            type: String,
+        },
+        //text- другой атрибут
+        text: {
+            required: true,
+            type: String,
+        }
+    },
+    methods: {
+        getClassName() {
+            return {
+                "alert-info": this.type === "info",
+                "alert-danger": this.type === "danger",
+                "alert-success": this.type === "success",
+            }
+        }
+    }
+});
+
+window.addEventListener("load", () => {
+    new Vue({
+        el: "#app",
+        components:{
+          "v-datepicker":vuejsDatepicker,
+          Editor,
+        },
+        data: {
+            posts: [],
+            showForm: false,
+            post:{
+                author:'',
+                date: '',
+                content:'',
+            }
+        },
+        methods: {
+            toggleFormVisibility(){
+                this.showForm = !this.showForm;
+            },
+            addPost(){
+                this.posts.push({
+                    author: this.post.author,
+                    date: this.post.date,
+                    content: this.post.content,
+                })
+            }
+        },
+        computed: {
+            showFormButtonCaption(){
+              return this.showForm ? "Скрыть форму":"Добавить форму";
+            },
+            showFormButtonClass(){
+              return {
+                  "btn-primary": !this.showForm,
+                  "btn-secondary": this.showForm
+              }
+            },
+        },
+    })
+});
